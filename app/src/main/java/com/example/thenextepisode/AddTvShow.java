b724141d9c95e6c90c1fcee83e3f32e7be082889
Sidhant Mishra <s9mishra@uwaterloo.ca>
2019-07-11 14:44:22 -0400

    Adding new Activity - next thing to do is add search functionality

---
+++ app/src/main/java/com/example/thenextepisode/AddTvShow.java
@@ -0,0 +1,34 @@
+package com.example.thenextepisode;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddTvShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tv_show);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
