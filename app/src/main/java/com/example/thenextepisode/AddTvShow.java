83dbc6c47c539b39afd8509f199db8877aa103bb
Sidhant Mishra <s9mishra@uwaterloo.ca>
2019-07-11 18:50:33 -0400

    Added Search bar in new activity

--- app/src/main/java/com/example/thenextepisode/AddTvShow.java
+++ app/src/main/java/com/example/thenextepisode/AddTvShow.java
@@ -4,7 +4,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class AddTvShow extends AppCompatActivity {
 
@@ -28,7 +30,16 @@ public class AddTvShow extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        MenuItem searchViewItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchViewItem.getActionView();
+
+        searchView.setIconifiedByDefault(false);
+
         return true;
     }
 }
