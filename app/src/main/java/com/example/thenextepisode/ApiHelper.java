010db7854d235e0e7eb92c7aef278dfc55e052b2
Sidhant Mishra <s9mishra@uwaterloo.ca>
2019-07-13 12:24:02 -0400

    Partial impl of actually making a tv show query

--- app/src/main/java/com/example/thenextepisode/ApiHelper.java
+++ app/src/main/java/com/example/thenextepisode/ApiHelper.java
@@ -39,6 +39,53 @@ public class ApiHelper {
                         try {
                             PreferenceManager.getDefaultSharedPreferences(context)
                                     .edit().putString("APIKEY", response.getString("token")).apply();
+                            Log.d("api", response.getString("token"));
+                        } catch (Exception ex) {
+                            Log.e("Response error: ", ex.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley: ", error.toString());
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                params.put("Accept", "application/json");
+
+                return params;
+            }
+        };
+
+        RequestQueueSingleton.getInstance(context).getRequestQueue().add(jsonObjectRequest);
+    }
+
+    static void searchForTvShow(final Context context, String tvShow) {
+        String url ="https://api.thetvdb.com/login";
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("apikey", "***REMOVED***");
+            obj.put("username", "***REMOVED***");
+            obj.put("userkey", "***REMOVED***");
+        } catch (Exception ex) {
+            Log.e("Couldn't put an object? ", ex.toString());
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url,
+                obj,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            PreferenceManager.getDefaultSharedPreferences(context)
+                                    .edit().putString("APIKEY", response.getString("token")).apply();
+                            Log.d("api", response.getString("token"));
                         } catch (Exception ex) {
                             Log.e("Response error: ", ex.toString());
                         }
