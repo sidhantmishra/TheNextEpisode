e3c49c4ea97d70dc3154add277750656a99f4903
Sidhant Mishra <s9mishra@uwaterloo.ca>
2019-07-14 22:40:38 -0400

    Well, it's something eh

---
+++ app/src/main/java/com/example/thenextepisode/ShowDao.java
@@ -0,0 +1,27 @@
+package com.example.thenextepisode;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ShowDao {
+    @Insert
+    public void insert(Show... shows);
+
+    @Update
+    public void update(Show... shows);
+
+    @Delete
+    public void delete(Show show);
+
+    @Query("SELECT * FROM show")
+    public List<Show> getAllShows();
+
+    @Query("DELETE FROM show")
+    public void deleteAllShows();
+}
